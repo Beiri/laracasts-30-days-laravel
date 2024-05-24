b872699705e7c147c027ee382f83f265ef71579c
Beiri Alife <beiri.al@outlook.com>
2024-05-24 01:11:19 -0300

    Ep 23

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Job;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class JobController extends Controller
 {
@@ -49,7 +52,7 @@ class JobController extends Controller
 
     public function update(Job $job)
     {
-        //authorize (On hold...)
+        Gate::authorize('edit-job', $job);
 
         request()->validate([
             'title' => ['required', 'min:3'],
@@ -66,7 +69,7 @@ class JobController extends Controller
 
     public function destroy(Job $job)
     {
-        //authorize (On hold...)
+        Gate::authorize('edit-job', $job);
 
         $job->delete();
 
